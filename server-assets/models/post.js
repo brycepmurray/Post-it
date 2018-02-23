d467bdedfa91c00d17f6c8bddb63cab41dc673ac
Benjamen King <brk.3rdof3kings@gmail.com>
2018-02-23 15:49:04 -0700

    finish 1st-draft server setup

--- server-assets/models/post.js
+++ server-assets/models/post.js
@@ -1,15 +1,17 @@
 var mongoose = require('mongoose')
 var Schema = mongoose.Schema
+var ObjectId = Schema.Types.ObjectId
 var schemaName = "Post"
 
 var schema = new Schema({
-  _id: String,
-  title: {type: String, required: true, default: 'A Title'},
-  desc:  {type: String, required: true, default: 'A Desc'},
+  title: {type: String, required: true},
+  desc:  {type: String, required: true},
   createdAt:  {type: Date, required: true, default: Date.now()},
-  imgUrl: {type: String, required: true, default: 'http://placehold.it/200x200'},
-  likes: {type: Number, required: true, min: 0, default: 4},
+  imgUrl: {type: String, required: true},
+  likes: {type: Number, required: true, min: 0},
+  dislikes: {type: Number, required: true, min: 0},
   userId: {type: ObjectId, required: true, ref: 'User'}
 })
+
 
 module.exports = mongoose.model(schemaName, schema)
--- server-assets/models/user.js
+++ server-assets/models/user.js
@@ -1,10 +1,8 @@
 var mongoose = require('mongoose')
 var Schema = mongoose.Schema
-var ObjectId = Schema.Types.ObjectId
 var schemaName = "User"
 
 var schema = new Schema({
-  _id: {type: String},
   name: {type: String, required: true},
   email: {type: String, required: true},
   password: {type: String, required: true},
