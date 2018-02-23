17a1d12d60cabddf1cd1bb3b708dfb88fa954b74
Trevor <trevor.spielman@gmail.com>
2018-02-23 12:22:21 -0700

    Basic Homepage Skeleton

--- www/src/components/Main.vue
+++ www/src/components/Main.vue
@@ -1,13 +1,93 @@
 <template>
-  <Navbar></Navbar>
   <div>
-    <h1>testing main</h1>
+      <Navbar></Navbar>
+    <div class="container-fluid">
+      <div class="row card-deck justify-content-around">
+        <div class="col-3 card p-2 m-4">
+          <img class="card-img-top" src="http://placehold.it/200X200" alt="">
+          <div class="card-body">
+            <h4>Title</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora in adipisci illum incidunt perferendis voluptates harum hic itaque ad. Expedita error ducimus cumque rerum soluta eligendi officia, provident aut delectus.</p>
+          </div>
+          <i class="card-footer far fa-thumbs-up fa-2x"></i>
+        </div>
+        <div class="col-3 card p-2 m-4">
+          <img class="card-img-top" src="http://placehold.it/200X200" alt="">
+          <div class="card-body">
+            <h4>Title</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora in adipisci illum incidunt perferendis voluptates harum hic itaque ad. Expedita error ducimus cumque rerum soluta eligendi officia, provident aut delectus.</p>
+          </div>
+          <i class="card-footer far fa-thumbs-up fa-2x"></i>
+        </div>
+        <div class="col-3 card p-2 m-4">
+          <img class="card-img-top" src="http://placehold.it/200X200" alt="">
+          <div class="card-body">
+            <h4>Title</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora in adipisci illum incidunt perferendis voluptates harum hic itaque ad. Expedita error ducimus cumque rerum soluta eligendi officia, provident aut delectus.</p>
+          </div>
+          <i class="card-footer far fa-thumbs-up fa-2x"></i>
+        </div>
+      </div>
+      <div class="row card-deck justify-content-around">
+        <div class="col-3 card p-2 m-4">
+          <img class="card-img-top" src="http://placehold.it/200X200" alt="">
+          <div class="card-body">
+            <h4>Title</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora in adipisci illum incidunt perferendis voluptates harum hic itaque ad. Expedita error ducimus cumque rerum soluta eligendi officia, provident aut delectus.</p>
+          </div>
+          <i class="card-footer far fa-thumbs-up fa-2x"></i>
+        </div>
+        <div class="col-3 card p-2 m-4">
+          <img class="card-img-top" src="http://placehold.it/200X200" alt="">
+          <div class="card-body">
+            <h4>Title</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora in adipisci illum incidunt perferendis voluptates harum hic itaque ad. Expedita error ducimus cumque rerum soluta eligendi officia, provident aut delectus.</p>
+          </div>
+          <i class="card-footer far fa-thumbs-up fa-2x"></i>
+        </div>
+        <div class="col-3 card p-2 m-4">
+          <img class="card-img-top" src="http://placehold.it/200X200" alt="">
+          <div class="card-body">
+            <h4>Title</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora in adipisci illum incidunt perferendis voluptates harum hic itaque ad. Expedita error ducimus cumque rerum soluta eligendi officia, provident aut delectus.</p>
+          </div>
+          <i class="card-footer far fa-thumbs-up fa-2x"></i>
+        </div>
+      </div>
+      <div class="row card-deck justify-content-around">
+        <div class="col-3 card p-2 m-4">
+          <img class="card-img-top" src="http://placehold.it/200X200" alt="">
+          <div class="card-body">
+            <h4>Title</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora in adipisci illum incidunt perferendis voluptates harum hic itaque ad. Expedita error ducimus cumque rerum soluta eligendi officia, provident aut delectus.</p>
+          </div>
+          <i class="card-footer far fa-thumbs-up fa-2x"></i>
+        </div>
+        <div class="col-3 card p-2 m-4">
+          <img class="card-img-top" src="http://placehold.it/200X200" alt="">
+          <div class="card-body">
+            <h4>Title</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora in adipisci illum incidunt perferendis voluptates harum hic itaque ad. Expedita error ducimus cumque rerum soluta eligendi officia, provident aut delectus.</p>
+          </div>
+          <i class="card-footer far fa-thumbs-up fa-2x"></i>
+        </div>
+        <div class="col-3 card p-2 m-4">
+          <img class="card-img-top" src="http://placehold.it/200X200" alt="">
+          <div class="card-body">
+            <h4>Title</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora in adipisci illum incidunt perferendis voluptates harum hic itaque ad. Expedita error ducimus cumque rerum soluta eligendi officia, provident aut delectus.</p>
+          </div>
+          <i class="card-footer far fa-thumbs-up fa-2x"></i>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+  import Navbar from './Navbar.vue'
 export default {
-  
+ components: {Navbar} 
 }
 </script>
 
--- www/src/components/Navbar.vue
+++ www/src/components/Navbar.vue
@@ -1,18 +1,29 @@
 <template>
-  <div class="col-4">
-<img src="../assets/images/IT-logo.png" alt="">
-<h1>Testing</h1>
+  <div class="row justify-content-between">
+    <div class="col-4">
+      <img class="resizeLogo" src="../assets/images/IT-logo.png">
+    </div>
+    <div class="col-4">
+      <h1>Post-IT</h1>
+    </div>
+    <div class="col-4 justify-item-end">
+      <h3>user profile information</h3>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
+  export default {
 
-}
+  }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
-
+  .resizeLogo {
+    height: 60px
+  }
+  *{
+    background: lightcoral
+  }
 </style>
