b02d029eef28962394c665eb200f68eec8b205a8
Trevor <trevor.spielman@gmail.com>
2018-02-23 11:47:14 -0700

    Removal of Node Modules

---
+++ www/src/components/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="col-4">
+<img src="../assets/images/IT-logo.png" alt="">
+<h1>Testing</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+
+</style>
--- www/src/router/index.js
+++ www/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Main from '@/components/Main'
+import Navbar from '@/components/Navbar'
 
 Vue.use(Router)
 
@@ -8,8 +9,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Main',
+      component: Main
+    },
+    {
+      path: '/',
+      name: 'Navbar',
+      component: Navbar
     }
   ]
 })
