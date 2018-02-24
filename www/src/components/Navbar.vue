7dcb016bbe9a2ce95a596cddf110690ed6da8bec
Trevor <trevor.spielman@gmail.com>
2018-02-23 20:41:21 -0700

    Test Commit after Merge issues

--- www/src/components/Navbar.vue
+++ www/src/components/Navbar.vue
@@ -2,11 +2,16 @@
   <div class="container-fluid">
     <div class="row d-flex justify-content-between navBar">
       <div class="col-lg-6 col-sm-12 logo">
-        <img class="resizeLogo" src="../assets/images/IT-logo.png"> 
-        <span class="title">Post-<b>IT</b></span>
+        <img class="resizeLogo" src="../assets/images/IT-logo.png">
+        <span class="title">Post-
+          <b>IT</b>
+        </span>
       </div>
       <div class="col-lg-6 col-sm-12 profileInfo">
-        <h3>user profile information</h3>
+        <a tabindex="0" class="btn btn-lg btn-danger" role="button" data-toggle="popover" data-trigger="focus" title="Sign In"
+          data-html = "true" data-content='<form action="submit">
+              <input type="text" name="username" placeholder="username" id="">
+            </form>'>Dismissible popover</a>
       </div>
     </div>
   </div>
@@ -14,34 +19,43 @@
 
 <script>
   export default {
-
+    mounted() {
+      $(function(){
+        $('[data-toggle="popover"]').popover()
+      })
+    }
   }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  .navBar{
+  .navBar {
     max-height: 5rem;
     box-shadow: 6px 6px 8px 2px rgba(109, 106, 106, 0.993);
   }
-  .logo{
+
+  .logo {
     align-items: center;
     display: flex;
   }
+
   .resizeLogo {
     height: 100%;
     max-width: 5rem;
   }
-  .profileInfo{
+
+  .profileInfo {
     justify-content: flex-end;
     display: flex;
     align-items: center
   }
-  .title{
+
+  .title {
     font-size: 35px;
     align-self: center;
   }
-  *{
+
+  * {
     background: rgb(220, 236, 250)
   }
 </style>
---
+++ www/src/store/index.js
@@ -0,0 +1,51 @@
+//main store file
+
+import vue from 'vue'
+import vuex from 'vuex'
+import axios from 'axios'
+
+var api = axios.create({
+  baseURL: '//localhost:3000/api/',
+  timeout: 3000
+})
+
+vue.use(vuex)
+
+export default new vuex.Store({
+  state: {
+    //DUMMY DATA
+    user: {},
+    posts: [{
+      "createdAt": "2018-02-23T23:18:02.415Z",
+      "_id": "5a90a683c0112e197c23478e",
+      "userId": "5a90a16cc0112e197c234788",
+      "title": "Find my parents",
+      "desc": "Help me find my biological parents, Jor-El is my father and my mother is Lara-El. They are on the planet Krypton",
+      "imgUrl": "https://vignette.wikia.nocookie.net/superman/images/f/f5/Krypton_-_Gods_and_Monsters.jpg",
+      "likes": 4,
+      "dislikes": 4,
+    }],
+    comments: [],
+  },
+  mutations: {
+    setPosts(state, payload) {
+      state.posts = payload
+    }
+  },
+
+  actions: {
+    addPost({ commit, dispatch }, payload) {
+      commit('addPost', payload)
+    },
+    getPosts({ commit, dispatch }, payload) {
+      api.get('posts')
+        .then(res => {
+          commit('setPosts', res.data)
+          console.log(res.data)
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    }
+  }
+})
