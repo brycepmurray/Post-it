e48cad213ad76e9edcb691ad4fd99c7cb1a8f236
Benjamen King <brk.3rdof3kings@gmail.com>
2018-02-23 18:50:09 -0700

    Clean up server JS files

--- server-assets/models/user.js
+++ server-assets/models/user.js
@@ -3,11 +3,25 @@ var Schema = mongoose.Schema
 var schemaName = "User"
 
 var schema = new Schema({
-  name: {type: String, required: true},
-  email: {type: String, required: true},
-  password: {type: String, required: true},
-  photoUrl: {type: String, required: true},
-  active: {type: Boolean}
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  photoUrl: {
+    type: String,
+    required: true
+  },
+  active: {
+    type: Boolean
+  }
 })
 
 module.exports = mongoose.model(schemaName, schema)
