207881f8a0dc398fb41af3f734194679856359e7
Bryce Murray <brycepmurray711@gmail.com>
2018-02-24 02:26:26 -0700

    yay username should be a function thing now

--- server-assets/models/post.js
+++ server-assets/models/post.js
@@ -5,39 +5,43 @@ var ObjectId = Schema.Types.ObjectId
 var schemaName = "Post"
 
 var schema = new Schema({
-  title: {
-    type: String,
-    required: true
-  },
-  desc: {
-    type: String,
-    required: true
-  },
-  createdAt: {
-    type: String,
-    required: true,
-    default: moment().format('MMMM Do YYYY, h:mm:ss a')
-  },
-  imgUrl: {
-    type: String,
-    required: true,
-    default: 'http://placehold.it/200X200'
-  },
-  likes: {
-    type: Number,
-    required: true,
-    min: 0
-  },
-  dislikes: {
-    type: Number,
-    required: true,
-    min: 0
-  },
-  userId: {
-    type: ObjectId,
-    required: true,
-    ref: 'User'
-  }
+    title: {
+        type: String,
+        required: true
+    },
+    desc: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: String,
+        required: true,
+        default: moment().format('MMMM Do YYYY, h:mm:ss a')
+    },
+    imgUrl: {
+        type: String,
+        required: true,
+        default: 'http://placehold.it/200X200'
+    },
+    likes: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+    dislikes: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+    userId: {
+        type: ObjectId,
+        required: true,
+        ref: 'User'
+    },
+    userName: {
+        type: String,
+        required: true,
+    }
 })
 
 module.exports = mongoose.model(schemaName, schema)
